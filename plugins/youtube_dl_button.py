ee6f7b1e4c2a045a631b997bf0b68d1285c7027b
ALEN TL <alentl5000@gmail.com>
2021-03-07 12:59:58 +0530

    Updated Harchoir

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -30,7 +30,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
-from hachoir.metadata import extractMetadata
+from hachoir.metadata.metadata import extractMetadata
 from hachoir.parser import createParser
 # https://stackoverflow.com/a/37631799/4723940
 from PIL import Image
