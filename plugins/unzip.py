048021cf6eeba4d455caa4a8e253f457b59cedce
ALEN TL <alentl5000@gmail.com>
2021-03-14 12:22:21 +0530

    Fixed Zip Modlue

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -28,6 +28,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
+from pyrogram.errors import MessageNotModified 
+
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["unzip"]))
 async def unzip(bot, update):
