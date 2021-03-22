7239229067f2224205e884fa58ff67eddeff2670
ALEN TL <alentl5000@gmail.com>
2021-03-23 03:29:42 +0530

    Commit Reverted

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -33,11 +33,11 @@ from helper_funcs.help_uploadbot import DownLoadFile
 
 @pyrogram.Client.on_message(pyrogram.filters.regex(pattern=".*http.*"))
 async def echo(bot, update):
-    if update.from_user.id not in Config.SUPER_DLBOT_USERS:
-        await bot.send_message(
+    if update.from_user.id not in Config.AUTH_USERS:
+        await bot.delete_messages(
             chat_id=update.chat.id,
-            text=Translation.NOT_AUTH_USER_TEXT
-            reply_to_message_id=update.message_id
+            message_ids=update.message_id,
+            revoke=True
         )
         return
     # logger.info(update)
