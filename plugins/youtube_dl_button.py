013dc864dba29976a80c72c0cebb9e84da31e4ae
p4uladam <81237180+p4uladam@users.noreply.github.com>
2021-03-24 09:17:14 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -234,11 +234,11 @@ async def youtube_dl_call_back(bot, update):
                 img = Image.open(thumb_image_path)
                 # https://stackoverflow.com/a/37631799/4723940
                 # img.thumbnail((90, 90))
-                if tg_send_type == "file":
-                    img.resize((320, height))
-                else:
-                    img.resize((90, height))
-                img.save(thumb_image_path, "JPEG")
+#                 if tg_send_type == "file":
+#                     img.resize((320, height))
+#                 else:
+#                     img.resize((90, height))
+#                 img.save(thumb_image_path, "JPEG")
                 # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
             else:
                 thumb_image_path = None
