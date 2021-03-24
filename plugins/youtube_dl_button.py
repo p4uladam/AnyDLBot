e87702aa2dec28fc10bfe0cae7b255b902ee4f20
p4uladam <81237180+p4uladam@users.noreply.github.com>
2021-03-24 09:08:21 +0530

    Commented #223

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -220,10 +220,10 @@ async def youtube_dl_call_back(bot, update):
                 width = 0
                 height = 0
                 metadata = extractMetadata(createParser(thumb_image_path))
-                if metadata.has("width"):
-                    width = metadata.get("width")
-                if metadata.has("height"):
-                    height = metadata.get("height")
+                # if metadata.has("width"):
+                width = metadata.get("width")
+                # if metadata.has("height"):
+                height = metadata.get("height")
                 if tg_send_type == "vm":
                     height = width
                 # resize image
